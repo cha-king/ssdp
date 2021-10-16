669675570caafad28027554cc5cf7ca352b52422
Charlie King <charlie.king@verkada.com>
2021-10-16 16:04:32 -0700

    Working

--- main.go
+++ main.go
@@ -2,47 +2,56 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
-	"os"
+
+	"golang.org/x/net/ipv4"
 )
 
 const ssdpAddress = "239.255.255.250:1900"
 
 func main() {
+
 	udpAddr, err := net.ResolveUDPAddr("udp4", ssdpAddress)
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println(udpAddr)
 
-	conn, err := net.ListenMulticastUDP("udp4", nil, udpAddr)
+	conn, err := net.ListenUDP("udp4", nil)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
+
+	pConn := ipv4.NewPacketConn(conn)
+	pConn.JoinGroup(nil, udpAddr)
 
 	request, err := http.NewRequest("M-SEARCH", "*", nil)
 	if err != nil {
 		panic(err)
 	}
 	request.Host = ssdpAddress
-	request.Header.Set("ST", "ssdp:all")
-	request.Header.Set("MAN", "ssdp:discover")
-	request.Header.Set("MX", "1")
+	request.Header["ST"] = []string{"ssdp:all"}
+	request.Header["MAN"] = []string{`"ssdp:discover"`}
+	request.Header["MX"] = []string{"5"}
 	raw, err := httputil.DumpRequest(request, true)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(string(raw))
 
-	_, err = conn.WriteToUDP(raw, udpAddr)
+	_, err = pConn.WriteTo(raw, nil, udpAddr)
 	if err != nil {
 		panic(err)
 	}
 
-	io.Copy(os.Stdout, conn)
+	buffer := make([]byte, 1024)
+	n, _, addr, err := pConn.ReadFrom(buffer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(addr)
+	fmt.Println(string(buffer[:n]))
 }
